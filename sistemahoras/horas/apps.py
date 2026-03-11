69cf66ee68531fb4094da30d3e7bab571f2b598d
abelNote <myctrick@gmail.com>
2026-03-11 16:47:24 -0300

    reestructura proyecto y elimina carpeta sistemahoras

--- sistemahoras/horas/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HorasConfig(AppConfig):
-    name = 'horas'
--- sistemahoras/horas/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import empleado
-
-class empleadoForm(forms.ModelForm):
-    class Meta:
-        model = empleado
-        fields = '__all__'
