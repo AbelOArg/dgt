6676c8d5e0deecae397aa5ed4c16564e1fe0e5bc
abelNote <myctrick@gmail.com>
2026-03-11 15:26:26 -0300

    Reestructura proyecto Django, en nueva carpeta backend

---
+++ backend/horas/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class empleado(models.Model):
+    id_empleado = models.AutoField(primary_key=True)
+    apellido = models.CharField(max_length=100, verbose_name='Apellido')
+    nombre = models.CharField(max_length=100, verbose_name='Nombre')
+    afiliado = models.IntegerField(verbose_name='Afiliado')
+    dni = models.IntegerField(verbose_name='DNI', null=True)
+    seccion = models.CharField(max_length=50,verbose_name='Seccion')
+    turno = models.CharField(max_length=20,verbose_name='Turno')
+    reparticion = models.CharField(max_length=50,verbose_name='Reparticion')
+    horasquetrabaja = models.IntegerField(verbose_name='Horas que trabaja')
+    horadeingreso = models.TimeField(verbose_name='Horario de ingreso', null=True)
+    horadeegreso = models.TimeField(verbose_name='Horario de salida', null=True)
+    is_active = models.BooleanField(default=True, verbose_name='Activo')
+
+    def __str__(self):
+        return f"{self.apellido}, {self.nombre} - Afiliado: {self.afiliado} - seccion: {self.seccion} - turno: {self.turno} - reparticion: {self.reparticion} - activo: {self.is_active}"
+    
+
+class horastrabajadasxdescando(models.Model):
+    id_horas = models.AutoField(primary_key=True)
+    empleado = models.ForeignKey(empleado, on_delete=models.CASCADE, verbose_name='Empleado')
+    fecha = models.DateField(verbose_name='Fecha')
+    horas_trabajadas = models.IntegerField(verbose_name='Horas trabajadas')
+
+    def __str__(self):
+        return f"Empleado: {self.empleado} - Fecha: {self.fecha} - Horas trabajadas: {self.horas_trabajadas}"
---
+++ backend/horas/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from . import views
+
+app_name ="horas"
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('mi_admin/', views.go_to_admin , name='mi_admin' ),
+    path('principal/', views.principal, name='principal'),
+    path('altaempleado/', views.altaempleado, name='altaempleado'),
+    path('buscarempleado/',views.buscarempleado,name="buscarempleado"),
+    path('modificarempleado/<int:afiliado>/',views.modificarempleado, name='modificarempleado'),
+    path('verdatosbuscar/',views.verdatosbuscar, name="verdatosbuscar"),
+    path('mostrarempleado/<int:afiliado>/',views.mostrarempleado, name="mostrarempleado"),
+    path('tipodecarga/', views.tipodecarga, name="tipodecarga"),
+    path('descanso/', views.descaso, name="descanso"),
+    path('extension/', views.extension, name="extension"),
+    path('secretaria/', views.secretaria, name="secretaria"),
+]
